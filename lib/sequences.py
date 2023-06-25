0899ee3ae955b5ea0ac58e897467b24c089abed9
SamuelWade <samuelrcwade@gmail.com>
2023-06-24 22:05:38 -0400

    lab completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
-
+import pdb
 def print_fibonacci(length):
-    pass
+    num_array = []
+    for i in range(length):
+        if i==0 or i==1:
+            num_array.append(i)
+        else:
+            new_value = num_array[i-1]+num_array[i-2]
+            num_array.append(new_value)
+        
+        # pdb.set_trace()
+        # num_array.append(i)
+    # pdb.set_trace()
+    print(num_array)
+    # return num_array
+        
+        
